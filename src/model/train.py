98d915096267e5bc973a7254fb8782184251d152
JoshForrest <45022702+JoshForrest@users.noreply.github.com>
2023-08-06 13:12:03 +1000

    Update train.py
    Correcting linting errors

--- src/model/train.py
+++ src/model/train.py
@@ -6,8 +6,9 @@ import os
 import mlflow
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression 
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+
 
 # change for pull trigger testing 2
 # define functions
@@ -36,9 +37,10 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
     return X_train, X_test, y_train, y_test
+
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
@@ -60,6 +62,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
